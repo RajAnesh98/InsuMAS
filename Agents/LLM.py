973657c98d8d9a64cf7b8b35328b586f0899fec2
huuthangly <lyhuuthang6270@gmail.com>
2025-10-02 17:44:21 -0400

    Create .env file

--- Agents/LLM.py
+++ Agents/LLM.py
@@ -1,6 +1,9 @@
 from Agents.utils import os
 from langchain_groq import ChatGroq
-# Set the environment variable with the CORRECT name
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env", override=True)
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 
 # The ChatGroq client will now find the key automatically
 llm = ChatGroq(model="moonshotai/kimi-k2-instruct-0905", temperature=0)
